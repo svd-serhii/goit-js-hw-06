31ea3e9e620ec34976a59d98d5bf45926531d01f
Serhii Demchenko <svdemchenko@gmail.com>
2022-10-27 22:58:41 +0300

    add task#8, task#9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === "" || password === "") {
+    return alert("Необхідно заповнити всі поля");
+  }
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset;
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const btnColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+btnColor.addEventListener("click", changeColor);
+
+function changeColor() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  textColor.textContent = randomColor;
 }
