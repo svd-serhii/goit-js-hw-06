8e238729278adc5c4d6beb2791a503d059bddec1
Serhii Demchenko <svdemchenko@gmail.com>
2022-10-30 19:56:21 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,16 +1,12 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 const ingredientsContainerEl = document.querySelector("#ingredients");
 
-let newList = [];
-
-const ingredientsLst = ingredients.map((element) => {
+const ingredientsList = ingredients.map((element) => {
   const ingredientsItem = document.createElement("li");
   ingredientsItem.classList.add("item");
   ingredientsItem.textContent = element;
 
-  newList.push(ingredientsItem);
+  ingredientsContainerEl.append(ingredientsItem);
 });
 
-ingredientsContainerEl.append(...newList);
-
-// console.log(ingredientsLst);
+// console.log(ingredientsList);
